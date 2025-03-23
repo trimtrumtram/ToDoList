3e355568c88bfbe48e69106862b5f0c6fd989ffb
trimtrumtram <146762260+trimtrumtram@users.noreply.github.com>
2025-03-23 14:55:27 +0300

    Add files via upload

---
+++ ToDo-app/src/main/java/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        TasksController controller = new TasksController();
+
+        while(true) {
+            controller.printMenu();
+
+            String choice = controller.userChoice();
+            switch (choice) {
+                case "1" -> controller.addTask();
+                case "2" -> controller.deleteTask();
+                case "3" -> controller.editTask();
+                case "4" -> controller.changeStatus();
+                case "5" -> controller.displayTasks();
+                case "6" -> controller.filterTasks();
+                case "7" -> controller.searchTask();
+                case "8" -> controller.showAnalytics();
+                case "9" -> controller.exit();
+                default -> System.out.println("Неверный выбор");
+            }
+        }
+    }
+}
